d1acd96d09d26babd908471ae727a06fb0ff35ab
Drew <dfriend414@gmail.com>
2020-05-19 22:24:45 -0400

    commented leds

--- hub.py
+++ hub.py
@@ -12,19 +12,19 @@ xshut.append(DigitalInOut(board.D21))
 xshut.append(DigitalInOut(board.D20))
 
 laser_base.set_addresses()
-leds.setup(board.D0, 30)
+#leds.setup(board.D0, 30)
 
 print("setup over")
 while enabled:
     if rio_coms.disabled():
         enabled = False
     else:
-        leds.normal_Rotation(4)
+        #leds.normal_Rotation(4)
         print("sending value")
         for i in range(len(lasers)):
             rio_coms.send_value(int(laser_base.distance(i)))
 
 
-leds.rainbow_cycle(0.01)
+#leds.rainbow_cycle(0.01)
 print("ended")
 laser_base.reset_addresses()
