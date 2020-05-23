6aa44712030f7c46e0983a9f5efaea085eeaf08c
Drew <dfriend414@gmail.com>
2020-05-22 22:07:39 -0400

    serial debugging

--- rio_coms.py
+++ rio_coms.py
@@ -2,7 +2,7 @@ import board
 import serial
 
 
-rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0, write_timeout = 0)
+rio = serial.Serial('/dev/ttyAMA0', 115200, timeout = 0, write_timeout = 0)
 #standin = rio.read()
 
 def disabled():
