7019c0d4ff5cea9ad24e3904e3715edbdbdde31f
Drew <dfriend414@gmail.com>
2020-07-31 18:56:24 -0400

    debugging

--- hub.py
+++ hub.py
@@ -9,7 +9,7 @@ lasers = laser_base.vl53
 enabled = True
 
 xshut.append(DigitalInOut(board.D19))
-#xshut.append(DigitalInOut(board.D26))
+xshut.append(DigitalInOut(board.D26))
 
 laser_base.reset_addresses()
 laser_base.set_addresses()
