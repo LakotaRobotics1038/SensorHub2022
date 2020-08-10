bcfc83ebeaeb6c8223a0eae2aa34845521f1792b
AndrewFriend8845 <Andrew.Friend8845@Butlertech.org>
2020-08-10 16:16:56 -0400

    added dio sensors, changed the purple

--- hub.py
+++ hub.py
@@ -1,29 +1,38 @@
 #v Imports/variables v#
+from collections import namedTuple
 from digitalio import DigitalInOut
 import board
 from Sensors import laser_base
+from Sensors import digitalSensors
 import rio_coms
 import animations
+
 lightCount = animations.qty
 xshut = laser_base.xshut
 lasers = laser_base.vl53
+dio = digitalSensors.digitalSensors
 enabled = True
 
 
 #v Between these are the only things you should change v#
 xshut.append(DigitalInOut(board.D21))
 xshut.append(DigitalInOut(board.D20))
+
+dio['limitSwitch'] = DigitalInOut(board.D18)
+
 lightCount = 10
 #^ Between these are the only things you should change ^#
 
 
 #code that runs during the match
+digitalSensors.pinSetup()
 laser_base.set_addresses()
 while enabled:
     if rio_coms.disabled():
         enabled = False
     else:
-        animations.rotate((255,0,255), (0,0,255), 4)
+        animations.rotate((125,0,255), (0,0,255), 4)
+        digitalSensors.sensorCheckOne()
         for i in range(len(lasers)):
             rio_coms.send_value(i, int(laser_base.distance(i)))
 
