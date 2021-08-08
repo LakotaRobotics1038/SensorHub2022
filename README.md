349ac4a5cb288e75542c193d3b08d136188947a8
Drew Friend <dfriend@gmail.com>
2021-08-09 00:52:43 +0100

    LEDS work, all animation methods are functional, pin 18 is an issue

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Other sensor possibilities:
     Gyro
     Accelerometer
     Line follower
+
+
+# When Setting Up New Raspberry Pi
+    use "pip3 install" with all necessary imports
+    enable i2c bus in the settings
+    set the hub program to run on boot in rc.local
--- hub.py
+++ hub.py
@@ -18,38 +18,44 @@ enabled = True
 
 listedShutoffs = [
     # DigitalInOut(board.D20),
-    # DigitalInOut(board.D21)
+    # DigitalInOut(board.D19)
     ]                                            #Add list of Laser XSHUT pins
+#xshut.append(listedShutoffs)                    #Keep commented unless using lasers
 
-#dio['limitSwitch'] = DigitalInOut(board.D18)    #copy to add digital sensors, board port is signal pin, change name
+#dio['limitSwitch'] = DigitalInOut(board.D16)    #copy to add digital sensors, board port is signal pin, change name
 
-# LED Signal Pin is set in animations, D18 to start
-lightCount = 9                                   #number of leds
-#animations.messageSetup("message here")         #converts your message to binary
-cycleDelay = 1
+# LED Signal Pin is set in animations, D21 to start
+lightCount = 10                                 #Number of leds
+rotate=True                                     #False if using message
+animations.messageSetup("Message Here")         #Converts your message to binary
+cycleDelay = 0.1
 
 #^ Between these are the only things you should change ^#
 
 
 #code that runs during the match
 digitalSensors.pinSetup()
-#xshut.append(listedShutoffs)
-#laser_base.set_addresses()
-animations.rotateSetup((100,0,255), (0,0,255), lightCount, 4)
+laser_base.set_addresses()
+animations.rotateSetup((125,0,200), (0,0,255), lightCount, 4)
 
 while enabled:
     if rio_coms.disabled():
         enabled = False
     else:
-        animations.rotate()                                  #only have one LED program active
-        #animations.message((125,0,255), (0,0,255))          #only have one LED program active
-        #print(digitalSensors.sensorCheck(0))
+        #input()                #only use when debugging, comment out otherwise
+
+        #v Only one animation program is active at a time v#
+        if rotate:                                             
+            animations.rotate()       
+        else:                          
+            animations.message((125,0,200), (0,0,255))          
+         #^ Only one animation program is active at a time ^#
+
         for i in range(len(lasers)):
             rio_coms.send_value(i+1, int(laser_base.distance(i)))
-        #input()                #only use when debugging, comment out otherwise
         time.sleep(cycleDelay)        #you may need a delay for the lights depending on the number of sensors, comment it out if you don't
 
 
 #This runs after the robot is disabled
 laser_base.reset_addresses()
-animations.rainbow_cycle(.1)
+animations.rainbow_cycle(0)
